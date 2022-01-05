1605f7e68307a7c57ac144d27e9785a7388b6b33
grigol.tvaradze <75114666+grigoltvaradze@users.noreply.github.com>
2022-01-05 14:31:20 +0400

    Add files via upload

---
+++ stream_davaleba/stream_davaleba/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace pathHomework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var from = @"C:\Users\ASUS\Desktop\from";
+            var to = @"C:\Users\ASUS\Desktop\to";
+            DateTime lastChangeTime = new DateTime(2022, 01, 04);
+            if (Directory.GetFiles(from).Length > 0)
+            {
+                var files = Directory.GetFiles(from);
+                foreach (var file in files)
+                {
+                    if(Directory.GetLastAccessTime(file) > lastChangeTime)
+                    {
+                        var fileName = Path.GetFileName(file);
+                        var destination = Path.Combine(to, fileName);
+                        File.Move(file, destination);
+                    }
+                    else
+                    {
+                        Console.WriteLine("you don't need this files");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Folder is empty");
+            }
+        }
+    }
+}
